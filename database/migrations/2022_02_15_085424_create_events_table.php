87f5b3e7c53cf967d338d848c0cdd0a9016ef4d5
imbranatoInformatico <dariomantovani@Mini-di-dario.homenet.telecomitalia.it>
2022-02-15 10:01:52 +0100

    inizio

---
+++ database/migrations/2022_02_15_085424_create_events_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateEventsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('events', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('idAmministratore');
+            $table->integer('codiceEvento');
+            $table->string('nome');
+            $table->string('tipo');
+            $table->string('descrizione');
+            $table->timestamps();
+            //qui eseguo la chiave esterna sulla tabella users
+            $table->foreign('idAmministratore')->references('id')->on('users')->onCascade('delete');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('events');
+    }
+}
