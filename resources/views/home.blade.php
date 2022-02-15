3c1ece4c634948543ce6e0231ae21902756ba48f
imbranatoInformatico <dariomantovani@Mini-di-dario.homenet.telecomitalia.it>
2022-02-15 10:46:32 +0100

    creata la migration per events e impostate la login come prima pagina

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,19 +4,22 @@
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
-
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    {{ __('You are logged in!') }}
-                </div>
-            </div>
+            <section class="loginWrap col-12">
+                <form class="form-gest "" action="firstStep.html">
+                    <label for="">Eventi esistenti </label>
+                    <select class="form-select" name="selectEventi" id="">
+                       @foreach ($events as $event)
+                           <option value="">{{ $event->nome }}</option>
+                       @endforeach
+                    </select>
+                    <input class="form-submit-button " type="submit" value="Conferma">
+                    <hr>
+                    <label for="">Crea un nuovo campionato o evento</label>
+                    <a href="nuovoEvento.html">
+                        <input class="form-submit-button " type="submit" value="Nuovo">
+                    </a>
+                </form>
+            </section>
         </div>
     </div>
 </div>
